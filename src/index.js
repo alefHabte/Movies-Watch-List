9320b646790f82002fc0c9ce0282ee84311d85f2
Alef <alefalmawossen@gmail.com>
2024-03-27 10:43:50 -0400

    the site is not functional but let me save my progress on git hub

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./components/StarRating";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        maxRating={5}
+        onSetRating={setMovieRating}
+      ></StarRating>
+      <p>This movie has {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
-    {/* <StarRating size={24} color="red" /> */}
+    {/* <StarRating
+      maxRating={5}
+      Messages={["bad", "good", "veryGood", "excellent", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test /> */}
 
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
 
